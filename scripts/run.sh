b3a67d337b1afc388b122b781d1613456bbc6836
A S M Iftekhar <40488794+ASMIftekhar@users.noreply.github.com>
2023-01-25 14:25:55 -0800

    Delete run.sh

--- scripts/run.sh
+++
@@ -1,6 +0,0 @@
-CUDA_VISIBLE_DEVICES=0 python2 main.py \
---first_word soa_vcoco \
---batch_size 8 \
---number_of_epochs 20 \
---learning_rate 0.001 \
---check_point gtnet2best --inference --resume  
